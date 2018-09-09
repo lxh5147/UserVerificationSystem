c1354ecadf71323605cd116331280c352e3ed9c7
royliu <lxh5147@126.com>
2018-09-09 16:35:25 +0800

    slightly reformat

--- model/cross_entropy_loss.py
+++ model/cross_entropy_loss.py
@@ -5,4 +5,4 @@ def cross_entropy_loss(labels, embeddings, num_classes):
     # labels: shape batch_size
     # embeddings: shape batch_size, dim
     logits = tf.layers.dense(inputs=embeddings, units=num_classes)
-    return tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits, )
+    return tf.losses.sparse_softmax_cross_entropy(labels=labels, logits=logits)
