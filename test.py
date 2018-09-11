465776d260af4a75412f97179049380e140f62bc
royliu <lxh5147@126.com>
2018-09-11 18:53:02 +0800

    added one simple test case to read a dataset

--- test.py
+++ test.py
@@ -6,12 +6,13 @@ from model.voice_dataset import read_audio
 
 def test_dataset():
     dataset = tf.data.Dataset.from_tensor_slices(np.array([1.0, 2.0, 3.0, 4.0, 5.0]))
+    dataset = dataset.shuffle(3)
     dataset = dataset.repeat().batch(2)
     iterator = dataset.make_one_shot_iterator()
     one_batch = iterator.get_next()
     i=0
     with tf.Session() as sess:
-        while i<4:
+        while i<8:
             print(sess.run(one_batch))
             i += 1
 
