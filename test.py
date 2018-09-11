8d4fcb553c5d3659e23dc53308791797e07821aa
royliu <lxh5147@126.com>
2018-09-11 16:34:00 +0800

    added one native test case for audio file reading

--- test.py
+++ test.py
@@ -1,9 +1,31 @@
 import numpy as np
+import tensorflow as tf
+import os
 
-a=[[1,2,3],[4,5,6]]
-b=[[1,2,3],[4,5,6]]
+from tensorflow.contrib.framework.python.ops import audio_ops as contrib_audio
+from tensorflow.python.ops import io_ops
 
-a=np.asanyarray(a)
-b=np.asanyarray(b)
-c=np.sum(a*b, axis=1)
-print (c)
+def test_audio_too_short():
+    desired_samples=100000
+    wav_file = "./data/train/121624931534904112937-0.wav"
+    wav_loader = io_ops.read_file(wav_file)
+    audio, sample_rate = contrib_audio.decode_wav(wav_loader,
+                                                  desired_channels=1)
+
+    # choose a random clip with desired_samples from the audio
+    all_samples = tf.shape(audio)[0]
+    audio = tf.cond(tf.less(all_samples, desired_samples),
+                    true_fn=lambda: tf.pad(tensor=audio,
+                                           paddings=[[0, desired_samples - all_samples], [0, 0]],
+                                           constant_values=-1),
+                    false_fn=lambda: tf.random_crop(value=audio, size=[desired_samples, 1])
+                    )
+    # update the static shape information of an audio tensor
+    audio.set_shape([desired_samples, 1])
+
+
+    with tf.Session() as sess:
+        all_samples_val, audio_val = sess.run([all_samples, audio])
+        print("length:{} padded length:{}".format(all_samples_val,len(audio_val)))
+
+test_audio_too_short()
